f93569bd415c895f9216b6cbab47130973c8c137
shahidul shohid <shahidulislamshohid7gmail.com>
2024-11-14 23:15:22 +0600

    login and register page implemenent is done

---
+++ src/pages/Register.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
+        <h2 className="text-center text-2xl font-semibold">
+          Register your account
+        </h2>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              placeholder="photo url"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-none">Register</button>
+          </div>
+        </form>
+        <p className="text-center font-semibold">
+          AllReady have an account ?{" "}
+          <Link className="text-red-500" to="/auth/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
